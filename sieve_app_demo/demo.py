0c16ee9c5a32dbd78abb4b5633afa9b9e22413f4
Ian Scott Knight <isk@alumni.stanford.edu>
2025-09-23 14:24:17 -0700

    sickness

--- sieve_app_demo/demo.py
+++ sieve_app_demo/demo.py
@@ -2,6 +2,6 @@ from sieve_core import get_logger
 
 def hello_demo():
     log = get_logger("demo")
-    log.info("Hello from sieve-app-demo, using sieve-core logger AHHHHHHHH!")
-    return "hello from sieve-app-demo AAAAAAAAAAAAAHHHHHHHHHHHH"
+    log.info("Hello from sieve-app-demo, using sieve-core logger AHHHHHHHH! Amber(AH)'s sick.")
+    return "hello from sieve-app-demo AAAAAAAAAAAAAHHHHHHHHHHHH. Amber (AH) is sick :("
 
