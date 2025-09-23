4ed8908be9def04a60c781724700628bdf100a66
Ian Scott Knight <isk@alumni.stanford.edu>
2025-09-23 14:20:04 -0700

    second try

--- sieve_app_demo/demo.py
+++ sieve_app_demo/demo.py
@@ -2,6 +2,6 @@ from sieve_core import get_logger
 
 def hello_demo():
     log = get_logger("demo")
-    log.info("Hello from sieve-app-demo, using sieve-core logger!")
+    log.info("Hello from sieve-app-demo, using sieve-core logger AHHHHHHHH!")
     return "hello from sieve-app-demo AAAAAAAAAAAAAHHHHHHHHHHHH"
 
