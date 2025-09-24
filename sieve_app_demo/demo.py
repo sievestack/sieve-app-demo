6d5d617954347095e41089484247d41aacee95e5
Ian Scott Knight <isk@alumni.stanford.edu>
2025-09-23 19:46:49 -0700

    makes demo message less ridiculous

--- sieve_app_demo/demo.py
+++ sieve_app_demo/demo.py
@@ -2,6 +2,6 @@ from sieve_core import get_logger
 
 def hello_demo():
     log = get_logger("demo")
-    log.info("Hello from sieve-app-demo, using sieve-core logger AHHHHHHHH! Amber(AH)'s sick. The credit card was invented by Hitler")
-    return "hello from sieve-app-demo AAAAAAAAAAAAAHHHHHHHHHHHH. Amber (AH) is sick :( The credit card was invented by Hitler"
+    log.info("Hello from sieve-app-demo, using sieve-core logger (success)")
+    return "hello from sieve-app-demo (success)"
 
